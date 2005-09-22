ae524ab99adc01fd5c4dd6e9423cd83e12213f77
David Coallier <davidc@php.net>
2005-09-22 17:41:44 +0000

    pretty much working proxy mode
    only problem seems to be HTML_AJAX.onError not being called when it should
    
    This might need merging over to cvs yet, fun
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_AJAX/trunk@196631 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/proxy_usage_inline_javascript.php
+++ docs/examples/proxy_usage_inline_javascript.php
@@ -75,6 +75,8 @@ callback.prototype = {
 // function used to clear out the target div
 function clearTarget() {
 	document.getElementById('target').innerHTML = 'clear';
+	document.getElementById('eventLog').innerHTML = 'clear';
+	document.getElementById('error').innerHTML = 'clear';
 }
 
 // register a custom error handler
@@ -90,19 +92,19 @@ HTML_AJAX.onError = function(e) {
 var onOpen = HTML_AJAX.onOpen;
 var onLoad = HTML_AJAX.onLoad;
 
-HTML_AJAX.onOpen = function(className,callName) {
+HTML_AJAX.onOpen = function(request) {
 	onOpen();
-	document.getElementById('eventLog').innerHTML += "Open: "+className+'::'+callName+"\n";
+	document.getElementById('eventLog').innerHTML += "Open: "+request.className+'::'+request.methodName+"\n";
 }
-HTML_AJAX.onSend = function(className,callName) {
-	document.getElementById('eventLog').innerHTML += "Send: "+className+'::'+callName+"\n";
+HTML_AJAX.onSend = function(request) {
+	document.getElementById('eventLog').innerHTML += "Send: "+request.className+'::'+request.methodName+"\n";
 }
-HTML_AJAX.onProgress = function(className,callName) {
-	document.getElementById('eventLog').innerHTML += "Progress: "+className+'::'+callName+"\n";
+HTML_AJAX.onProgress = function(request) {
+	document.getElementById('eventLog').innerHTML += "Progress: "+request.className+'::'+request.methodName+"\n";
 }
-HTML_AJAX.onLoad = function(className,callName) {
+HTML_AJAX.onLoad = function(request) {
 	onLoad();
-	document.getElementById('eventLog').innerHTML += "Load: "+className+'::'+callName+"\n";
+	document.getElementById('eventLog').innerHTML += "Load: "+request.className+'::'+request.methodName+"\n";
 }
 
 </script>

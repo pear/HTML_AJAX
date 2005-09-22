0c9c55d4ac5971e46237e76e2100b79993530de2
David Coallier <davidc@php.net>
2005-09-22 17:32:13 +0000

    pretty much working proxy mode
    only problem seems to be HTML_AJAX.onError not being called when it should
    
    This might need merging over to cvs yet, fun
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_AJAX/trunk@196627 c90b9560-bf6c-de11-be94-00142212c4b1

--- js/Dispatcher.js
+++ js/Dispatcher.js
@@ -5,15 +5,14 @@
  * @package    AJAX
  * @author     Joshua Eichorn <josh@bluga.net>
  * @copyright  2005 Joshua Eichorn
- * @copyright  2004-2005 Harry Fuecks
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
- * @todo	build urls smartly in doCall
+ * @license    lgpl
  */
-function HTML_AJAX_Dispatcher(className,mode,callback,serverUrl,serializer,unserializer) 
+function HTML_AJAX_Dispatcher(className,mode,callback,serverUrl,serializerType) 
 {
 	this.className = className;
 	this.mode = mode;
 	this.callback = callback;
+    this.serializerType = serializerType;
 
 	if (serverUrl) {
 		this.serverUrl = serverUrl
@@ -21,45 +20,13 @@ function HTML_AJAX_Dispatcher(className,mode,callback,serverUrl,serializer,unser
 	else {
 		this.serverUrl = window.location;
 	}
-
-	if (serializer) {
-		eval("this.serialize = new HTML_AJAX_Serialize_"+serializer+";");
-	}
-	else {
-		this.serialize = new HTML_AJAX_Serialize_JSON();
-	}
-	if (unserializer) {
-		eval("this.unserialize = new HTML_AJAX_Serialize_"+unserializer+";");
-	}
-	else {
-		this.unserialize = new HTML_AJAX_Serialize_JSON();
-	}
 }
 
 HTML_AJAX_Dispatcher.prototype = {
     /**
-     * HTML_AJAX_Http_Client instance
-     */
-	client: null,
-
-    /**
      * Timeout for async calls
      */
 	timeout: 20000,
-
-    /**
-     * Request object instance
-     */
-	request: false,
-
-    /**
-     * Create a new HTML_AJAX_HttpClient
-     */
-	initClient: function() 
-    {
-		this.client = new HTML_AJAX_HttpClient();
-        this.client.dispatcher = this;
-	},
 
     /**
      * Make an ajax call
@@ -69,149 +36,28 @@ HTML_AJAX_Dispatcher.prototype = {
      */
 	doCall: function(callName,args) 
     {
-		if ( !this.client ) {
-		    this.initClient();
-		}
-		if ( !this.request ) {
-			this.request = new HTML_AJAX_Request(this.serialize);
-		}
-		this.request.reset();
-		this.request.serverurl = this.serverUrl+"?c="+this.className+"&m="+callName;
-		this.timeout = this.timeout;
+        var request = new HTML_AJAX_Request();
+		request.requestUrl = this.serverUrl;
+        request.className = this.className;
+        request.methodName = callName;
+		request.timeout = this.timeout;
+        request.contentType = this.contentType;
+        request.serializer = eval('new HTML_AJAX_Serialize_'+this.serializerType);
         
 		for(var i=0; i < args.length; i++) {
-		    this.request.addArg(i,args[i]);
+		    request.addArg(i,args[i]);
 		};
-        
+
 		if ( this.mode == "async" ) {
-		    return this._asyncCall(this.request,callName);
+		    request.isAsync = true;
 		} else {
-		    return this._syncCall(this.request,callName);
+		    request.isAsync = false;
 		}
-	},
 
-	/**
-     * Call remote procedure asynchronously
-     * @param   object  request an instance of HTML_AJAX_Request
-     * @param   string  callName    the method being called
-	 * @access private
-     */
-	_asyncCall: function(request, callName) 
-    {
-		try {
-			this.client.asyncCall(request,this,callName);
-		} catch (e) {
-			this.errorFunc(e);
-		}
-		return;
-	},
-    
-	/**
-     * Call remote procedure synchronously
-     * @param   object  request an instance of HTML_AJAX_Request
-     * @param   string  callName    the method being called
-	 * @access private
-     */
-	_syncCall: function(request, callName) 
-    {
-		try {
-			var response = this.client.call(request,callName);
+        var self = this;
+        request.callback = function(result) { self.callback[callName](result); }
 
-			try {
-                this._setupUnserializer(this.client.xmlhttp.getResponseHeader('Content-Type'));
-				var data = this.unserialize.unserialize(response);
-                
-				try {
-					return data;
-				} catch (e) {
-                
-					if ( e.name == 'Server_Error' ) {
-						this.serverErrorFunc(e);
-					} else {
-						this.applicationErrorFunc(e);
-					}
-
-				}
-
-			} catch (e) {
-				e.name = 'Server_Error';
-				e.code = 2006;
-				e.response = response;
-				this.errorFunc(e);
-			}
-
-		} catch(e) {
-			this.errorFunc(e);
-		}
-	},
-    
-    /**
-     * Event handler for load event from the HttpClient
-     * 
-     * If you want to do something based on this event checkout HTML_AJAX.onLoad instead of overriding this method
-     *
-     * @param   mixed   response
-     * @param   string  callName    method that was called
-     * @param   string  contentType Content-type from the http header
-     */
-    onLoad: function(response, callName, contentType) 
-    {
-        try {
-            this._setupUnserializer(contentType);
-            var data = this.unserialize.unserialize(response);
-            try {
-                    
-                if ( this.callback[callName] ) {
-                    try {
-                        this.callback[callName](data);
-                    } catch(e) {
-                        // Error in handler method (e.g. syntax error) - display it
-                        this.errorFunc(e);
-                    }
-                } else {
-                    alert('Your handler must define a method '+callName);
-                }
-
-                } catch (e) {
-
-                    e.client = this.className;
-                    e.call = callName;
-                    
-                    var errorFunc = callName+'Error';
-                    
-                    if ( this.callback[errorFunc] ) {
-                        try {
-                            this.callback[errorFunc](e);
-                        } catch(e) {
-                            // Error in handler method (e.g. syntax error) - display it
-                           this.errorFunc(e);
-                        }
-                    } else {
-                       this.errorFunc(e);
-                    }
-                }
-
-            } catch (e) {
-                e.name = 'Server_Error';
-                e.code = 2006;
-                e.response = response;
-                e.client = this.className;
-                e.call = callName;
-                this.errorFunc(e);
-            }
-    },
-
-    /**
-     * Handle an error using the global handler if it exists else rethrows the exception
-     * @param   object  e   an error object
-     */
-	errorFunc: function(e) {
-        if (HTML_AJAX.onError) {
-            HTML_AJAX.onError(e);
-        }
-        else {
-            throw(e);
-        }
+        return HTML_AJAX.makeRequest(request);
 	}
 };
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */

cda6298b0029337b2ed943be95251ccf0898fa23
Joshua Eichorn <jeichorn@php.net>
2005-09-27 15:26:39 +0000

    Update license on docs
    git-svn-id: http://svn.php.net/repository/pear/packages/HTML_AJAX/trunk@197063 c90b9560-bf6c-de11-be94-00142212c4b1

--- docs/examples/proxyless_usage.php
+++ docs/examples/proxyless_usage.php
@@ -21,7 +21,7 @@
  * @package    AJAX
  * @author     Joshua Eichorn <josh@bluga.net>
  * @copyright  2005 Joshua Eichorn
- * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @license    http://www.opensource.org/licenses/lgpl-license.php  LGPL
  * @version    Release: @package_version@
  * @link       http://pear.php.net/package/HTML_AJAX
  */
